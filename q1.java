bffeff79253eb4f0e2c65e0832ebe7c0101e14cf
Rochak <np01cp4a250201@islingtoncollege.edu.np>
2025-12-02 14:45:08 +0545

    Initial sharing of project

---
+++ q1.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class q1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class q1
+{
+    public static void main(String[] args)
+    {
+        char n="rochak";
+        System.out.println("n");
+        
+    }
+    
+}
